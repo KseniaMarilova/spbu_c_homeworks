bf81204f375ee7532abf41a832662baae8bf6ae2
KseniaMarilova <ksenia_marilova@mail.ru>
2021-09-20 20:49:37 +0300

    add task1.3

---
+++ homework1/task1.3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+
+int main()
+{
+    int n = 0, m = 0;
+    printf("Input intgers m, n \n");
+    scanf("%d%d", &m, &n);
+    int *array = calloc(n+m, sizeof(int));
+    printf("Input %d integers\n", n+m);
+    for (int i = 0; i < n + m; i++)
+        scanf("%d", &array[i]);
+    for (int i = m - 1; i >= 0; i--)
+        for (int j = i; j < i + n; j++)
+            swap(&array[j], &array[j + 1]);
+    printf("Start of array of length %d and end of length %d were swapped: \n", m, n);
+    for (int i = 0; i < n + m; i++)
+        printf("%d ", array[i]);
+    free(array);
+}
+
