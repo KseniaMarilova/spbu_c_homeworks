12efc70690094809c18996c73bbab98506d3ae97
KseniaMarilova <ksenia_marilova@mail.ru>
2021-09-21 17:31:32 +0300

    style fix

--- homework1/task1.5.c
+++ homework1/task1.5.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
- void printLeft(int left[], int *size, int *stage)
- {
-     for(int i = 0; i < (*size - *stage) / 2; i++) {
-         printf("%5d", left[i]);
-         left[i]--;
+void printLeft(int left[], int* size, int* stage)
+{
+    for (int i = 0; i < (*size - *stage) / 2; i++) {
+        printf("%5d", left[i]);
+        left[i]--;
      }
 }
-void printRight(int right[], int *size, int *stage)
+void printRight(int right[], int* size, int* stage)
 {
-    for(int i = (*size - *stage) / 2 - 1; i >= 0; i--) {
+    for (int i = (*size - *stage) / 2 - 1; i >= 0; i--) {
         printf("%5d", right[i]);
         right[i]++;
     }
@@ -21,14 +21,14 @@ int main()
     printf("Input integer number n \n");
     scanf("%d", &size);
     printf("Numbers from 1 to n*n in a spiral: \n");
-    int *left = calloc(size / 2 + 1, sizeof(int));
-    int *right = calloc(size / 2 + 1, sizeof(int));
+    int* left = calloc(size / 2 + 1, sizeof(int));
+    int* right = calloc(size / 2 + 1, sizeof(int));
     int stage = size;
     while (stage >= 1) {
         printLeft(left, &size, &stage);
         left[(size - stage) / 2] = stage * (stage - 1);
         right[(size - stage) / 2] = (stage - 2) * (stage - 2) + 1;
-        for(int i = 0; i < stage; i++)
+        for (int i = 0; i < stage; i++)
             printf("%5d", stage * (stage - 1) + 1 + i);
         printRight(right, &size, &stage);
         printf("\n");
@@ -42,7 +42,7 @@ int main()
         printRight(right, &size, &stage);
         stage += 2;
         printf("\n");
-     }
+    }
     free(left);
     free(right);
- }
+}
