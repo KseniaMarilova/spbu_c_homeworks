6b49fc51e83dd758d5546c368b3b6de6f8886122
KseniaMarilova <ksenia_marilova@mail.ru>
2021-09-21 17:06:38 +0300

    style fix

--- homework1/task1.4.c
+++ homework1/task1.4.c
@@ -2,13 +2,14 @@
 
 void outputPrimeNumbers(int highestNumber)
 {
-    int *is—omposite = calloc(highestNumber+1, sizeof(int));
+    int* is—omposite = calloc(highestNumber + 1, sizeof(int));
     int div = 2;
     while (div <= highestNumber) {
         for (int i = 2 * div; i <= highestNumber; i += div)
             is—omposite[i] = 1;
         div++;
-        while (is—omposite[div]) div++;
+        while (is—omposite[div])
+            div++;
     }
     printf("Prime numbers not exceeding %d: \n", highestNumber);
     for (int i = 2; i <= highestNumber; i++)
@@ -17,7 +18,8 @@ void outputPrimeNumbers(int highestNumber)
     free(is—omposite);
 }
 
-int main() {
+int main()
+{
     printf("Input integer number\n");
     int highestNumber = 0;
     scanf("%d", &highestNumber);
