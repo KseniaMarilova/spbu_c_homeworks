9a21f128639bdb0123e639e069b102a546eb882c
KseniaMarilova <ksenia_marilova@mail.ru>
2021-09-29 21:31:00 +0300

    fixed header path

--- library/LinkedMap/map.c
+++ library/LinkedMap/map.c
@@ -1,10 +1,10 @@
+#include "map.h"
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include "../map.h"
 
 struct LinkedMapElement {
     char key[128];
