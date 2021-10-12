be9b084ff4f6dbb79c5f17eca8f3038d99ad6111
KseniaMarilova <ksenia_marilova@mail.ru>
2021-10-12 19:04:14 +0300

    add task

--- homework3/task1.c
+++ homework3/task1.c
@@ -1,4 +1,22 @@
-//
-// Created by ksen4 on 12.10.2021.
-//
+#include <math.h>
+#include <stdio.h>
 
+int main() {
+    double mantissa = 0;
+    printf("Enter a number:");
+    scanf("%lf", &mantissa);
+    int exponent = 0;
+    while (fabs(mantissa) < 1) {
+        mantissa *= 2;
+        exponent--;
+    }
+    while (fabs(mantissa) > 2) {
+        mantissa /= 2;
+        exponent++;
+    }
+    char mantissaSign = '+';
+    if (mantissa < 0)
+        mantissaSign = '-';
+    printf("Result: %c%g*2^%d\n", mantissaSign, fabs(mantissa), exponent);
+    return 0;
+}
