10f778be3b7ac9f44890c9d7efea7a2bc556fdb6
KseniaMarilova <ksenia_marilova@mail.ru>
2021-11-07 19:54:49 +0300

    new solution of task3.2

--- homework3/task3.2.c
+++ homework3/task3.2.c
@@ -1,30 +1,60 @@
-#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void printNormalizedForm(double number)
+typedef union Number {
+    double value;
+    char binaryForm[8];
+} Number;
+
+int getBit(char number, int position)
 {
-    double mantissa = number;
+    char bit = (number >> position) & 0b1;
+    return bit;
+}
+
+int getExponent(Number number)
+{
+    int power = 1;
     int exponent = 0;
-    while (fabs(mantissa) < 1) {
-        mantissa *= 2;
-        exponent--;
+    for (int i = 4; i < 8; i++) {
+        exponent += power * getBit(number.binaryForm[6], i);
+        power <<= 1;
     }
-    while (fabs(mantissa) > 2) {
-        mantissa /= 2;
-        exponent++;
+    for (int i = 0; i < 7; i++) {
+        exponent += power * getBit(number.binaryForm[7], i);
+        power <<= 1;
     }
-    char mantissaSign = '+';
-    if (mantissa < 0)
-        mantissaSign = '-';
-    printf("%c%g*2^%d", mantissaSign, fabs(mantissa), exponent);
+    return exponent - 1024 + 1;
+}
+
+double getMantissa(Number number)
+{
+    double power = 0.5;
+    double mantissa = 1;
+    for (int i = 3; i >= 0; i--) {
+        mantissa += power * getBit(number.binaryForm[6], i);
+        power /= 2;
+    }
+    for (int j = 5; j >= 0; j--)
+        for (int i = 7; i >= 0; i--) {
+            mantissa += power * getBit(number.binaryForm[j], i);
+            power /= 2;
+        }
+    return mantissa;
+}
+
+char getSign(Number number)
+{
+    if (getBit(number.binaryForm[7], 7))
+        return '-';
+    return '+';
 }
 
 int main()
 {
-    double number = 0;
+    Number number;
     printf("Enter a number:");
-    scanf("%lf", &number);
-    printf("Result: ");
-    printNormalizedForm(number);
+    scanf("%lf", &number.value);
+    printf("Result:  %c%g*2^%d", getSign(number), getMantissa(number), getExponent(number));
     return 0;
 }
