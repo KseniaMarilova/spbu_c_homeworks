21cd425ffebab8335b1428d2513b3af07c56c931
KseniaMarilova <ksenia_marilova@mail.ru>
2021-12-21 12:23:15 +0300

    add

--- final/task1.c
+++ final/task1.c
@@ -1,3 +1,26 @@
-//
-// Created by ksen4 on 21.12.2021.
-//
+#include "../library/TreeMap/TreeMap.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number = 0, newCount = 0;
+    TreeMap* map = createTreeMap(compare);
+    scanf("%d", &number);
+    while (number) {
+        Value count = get(map, wrapInt(number));
+        if (!isNone(count))
+            newCount = getInt(get(map, wrapInt(number))) + 1;
+        else
+            newCount = 1;
+        put(map, wrapInt(number), wrapInt(newCount));
+        scanf("%d", &number);
+    }
+    TreeMapIterator* iterator = getIterator(map);
+    while (hasElement(iterator)) {
+        printf("%d %d\n", getInt(getIteratorKey(iterator)), getInt(getIteratorValue(iterator)));
+        next(iterator);
+    }
+    freeTreeMapIterator(iterator);
+    deleteTreeMap(map);
+}
